6b963084f3ebe2242dcf76ed310353fcf403e4bb
V-Vaal <valstaure@gmail.com>
2025-07-14 23:28:22 +0200

    config(wagmi): setup RainbowKit avec Sepolia ETH + localhost

---
+++ frontend/constants/index.ts
@@ -0,0 +1,18 @@
+// constants/index.ts
+
+// 🔐 Adresses des contrats déployés localement (Hardhat)
+// 💡 À mettre à jour après chaque déploiement local
+// 📝 Ne pas utiliser en prod/testnet sans adaptation
+
+export const vaultAddress = '0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE'
+export const tokenRegistryAddress = '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'
+
+export const mockTokenAddresses = {
+  mUSDC: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  mGOLD: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+  mBTC: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+  mBONDS: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+  mEQUITY: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+}
+
+export const mockOracleAddress = '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e'
